c93865d9a94ff050f69001ddb9e3ea46bc0535bb
Vaitu0 <pawel.dubiel0880@gmail.com>
2026-09-24 15:20:44 +0200

    changes to app

--- apps/test/app.py
+++ apps/test/app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonife
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Hello, World! This is a test app."
+
+@app.route('/health')
+def health():
+    return jsonife({"status": "ok"})
+
+@app.route('/<name>')
+def user(name):
+    return f"Hello, {name}! This is a test app."
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
